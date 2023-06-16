e21cfa094a1afd4a499e2603079625ea0db14ef2
曾进 <zengjin@88.com>
2023-06-16 11:31:47 +0800

    Create focusclock.py

---
+++ focusclock.py
@@ -0,0 +1,13 @@
+import time
+
+def countdown(t):
+    while t:
+        mins, secs = divmod(t, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end="\r")
+        time.sleep(1)
+        t -= 1
+    print('专注时间结束！')
+
+t = 25 * 60 # 25 minutes
+countdown(t)
